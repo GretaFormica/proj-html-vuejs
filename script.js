f964d95c4312519d2a43770ff1337a94e27c6629
GretaFormica <greta.formica@gmail.com>
2023-02-21 12:42:57 +0100

    script links

--- script.js
+++ script.js
@@ -3,6 +3,32 @@ const app = Vue.createApp({
     data() {
 
         return {
+
+            utils: {
+                num: "+1 (305) 1234-5678",
+                mail: "hello@example.com",
+                address: "main avenue, 987"
+            },
+            
+            links: [
+
+                "home",
+                "services",
+                "about",
+                "projects",
+                "result",
+                "get in touch"
+
+            ],
+
+            about: [
+                "the company",
+                "institutional",
+                "social & events",
+                "innovation",
+                "environment",
+                "tecnology",
+            ]
             
         }
     },
